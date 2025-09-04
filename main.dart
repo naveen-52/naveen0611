2795577a6c24548dfd6bf8e4722404a5b746e49b
naveen-52 <naveenit811@gmail.com>
2025-09-04 23:10:22 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -5,133 +5,128 @@ void main() {
 }
 
 class MainApp extends StatefulWidget {
-   MainApp({super.key});
+  MainApp({super.key});
 
   @override
   State<MainApp> createState() => _MainAppState();
 }
 
 class _MainAppState extends State<MainApp> {
- double  size = 0;
- String inputValue="";
- String calculatorvalue="";
- String operat="";
+  double size = 0;
+  String outputvalue = "";
+  String inputvalue = "";
+  String operator = "";
+
   @override
   Widget build(BuildContext context) {
-   size=MediaQuery.of(context).size.width/5;
+    size = MediaQuery.of(context).size.width / 5;
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(255, 250, 250, 250),
         body: Column(
           children: [
             Container(
               alignment: Alignment.bottomRight,
-              child:Text(inputValue,style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0),fontSize:100),)
+              child: Text(
+                inputvalue,
+                style: TextStyle(color: Colors.black, fontSize: 100),
+              ),
             ),
             Column(
               children: [
                 Row(
                   children: [
-                    calbutton("7",Colors.white),
-                     calbutton("8",Colors.white),
-                      calbutton("9",Colors.white),
-                       calbutton("/",const Color.fromARGB(255, 255, 94, 45)),
-                       ],
-                    ),
-                     Row(
-                  children: [
-                    calbutton("4",Colors.white),
-                     calbutton("5",Colors.white),
-                      calbutton("6",Colors.white),
-                       calbutton("*",const Color.fromARGB(255, 255, 94, 45)),
-                       ],
-                    ),
-                     Row(
-                  children: [
-                    calbutton("1",Colors.white),
-                     calbutton("2",Colors.white),
-                      calbutton("3",Colors.white),
-                       calbutton("-",const Color.fromARGB(255, 255, 94, 45)),
-                       ],
-                    ),
-                     Row(
-                  children: [
-                    calbutton("0",Colors.white),
-                     calbutton(".",Colors.white),
-                      calbutton("=",Colors.white),
-                       calbutton("+",const Color.fromARGB(255, 255, 94, 45)),
-                       ],
-                    ),
+                    buildcircle("7", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("8", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("9", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("/", const Color.fromARGB(255, 255, 255, 255)),
                   ],
                 ),
-                calbutton("clear", const Color.fromARGB(255, 255, 255, 255)),
+                Row(
+                  children: [
+                    buildcircle("4", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("5", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("6", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("*", const Color.fromARGB(255, 255, 255, 255)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildcircle("1", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("2", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("3", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("-", const Color.fromARGB(255, 255, 255, 255)),
+                  ],
+                ),
+                Row(
+                  children: [
+                    buildcircle("0", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle(".", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("=", const Color.fromARGB(255, 255, 255, 255)),
+                    buildcircle("+", const Color.fromARGB(255, 255, 255, 255)),
+                  ],
+                ),
+                buildcircle("clear", const Color.fromARGB(255, 255, 255, 255)),
               ],
             ),
+          ],
         ),
-      );
+      ),
+    );
   }
 
-Widget calbutton(String text,Color bgColor)
-{
-  return InkWell(
-    onTap: () {
-      if(text=="clear")
-      {
-setState(() {
-  inputValue="";
-  calculatorvalue="";
-  operat="";
-});
-      }
-      else if(text=="="){
-        setState(() {
-          double calc=double.parse(calculatorvalue);
-          double input=double.parse(inputValue);
-          //inputValue=calculatorvalue+inputValue;
-         if(operat=="+")
-         {
-          inputValue=(calc+input).toString();
-         }
-         else if(operat=="-")
-         {
-          inputValue=(calc-input).toString();
-         }
-         else if(operat=="*")
-         {
-          inputValue=(calc*input).toString();
-         }
-         else if(operat=="/")
-         {
-          inputValue=(calc/input).toString();
-         }
-        });
-      }
-      else if(text=="+"||text=="-"||text=="*"||text=="/")
-      {
-       setState(() {
-          calculatorvalue=inputValue;
-          inputValue="";
-          operat=text;
-       });
-      }
-      else{
-setState(() {
-        inputValue=inputValue+text;//concatenate panna use aguthu
-      });
-      }
-      
-    },
-    child: Container(
-                        decoration: BoxDecoration(
-                          color:bgColor,
-                          borderRadius: BorderRadius.circular(100),
-                        ),
-                        margin: EdgeInsets.all(8.5),
-                        width: size,
-                        height: size,
-                        alignment: Alignment.center,
-                        child:  Text(text,style: TextStyle(color: Color.fromARGB(255, 0, 0, 0),fontSize: 30))
-                      ),
-  );
-}
+  Widget buildcircle(String number, Color bgColor) {
+    return InkWell(
+      onTap: () {
+        if (number == "clear") {
+          setState(() {
+            inputvalue = "";
+            outputvalue = "";
+            operator = "";
+          });
+        } else if (number == "=") {
+          setState(() {
+            double num1 = double.parse(outputvalue);
+            double num2 = double.parse(inputvalue);
+            if (operator == "+") {
+              inputvalue = (num1 + num2).toString();
+            } else if (operator == "-") {
+              inputvalue = (num1 - num2).toString();
+            } else if (operator == "*") {
+              inputvalue = (num1 * num2).toString();
+            } else if (operator == "/") {
+              inputvalue = (num1 / num2).toString();
+            }
+          });
+        }
+        else if (number == "+" ||
+            number == "-" ||
+            number == "*" ||
+            number == "/") {
+          setState(() {
+            outputvalue = inputvalue;
+            inputvalue = "";
+            operator = number;
+          });
+        } else {
+          setState(() {
+            inputvalue += number;
+          });
+        }
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: bgColor,
+          borderRadius: BorderRadius.circular(100),
+        ),
+        margin: EdgeInsets.all(8),
+        height: size,
+        width: size,
+        alignment: Alignment.center,
+        child: Text(
+          number,
+          style: TextStyle(color: Colors.black, fontSize: 30),
+        ),
+      ),
+    );
+  }
 }
